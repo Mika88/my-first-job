8e56115ddc80667f72732968735dda933d2a3eda
Mika88 <mikaspivak@gmail.com>
2019-08-05 11:11:06 -0700

    Add show and new actions to JobPostsController, add a private job_post_params method

--- app/controllers/job_posts_controller.rb
+++ app/controllers/job_posts_controller.rb
@@ -3,4 +3,19 @@ class JobPostsController < ApplicationController
       @job_posts = JobPost.all
       render json: @job_posts
     end
+
+    def show
+        @job_post = JobPost.find(params[:id])
+        render json: @job_post
+    end
+
+    def new
+        @job_post = JobPost.new
+    end
+
+    private
+
+    def job_post_params
+        params.require(:job_post).permit(:post_type, :job_type, :description, :location, :salary, :creator_email, :creator_name)
+    end
 end
