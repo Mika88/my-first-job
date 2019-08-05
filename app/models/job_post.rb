94d6ab29c82f9043c75f064f3d241f07abc52e0b
Mika88 <mikaspivak@gmail.com>
2019-08-05 10:17:12 -0700

    Add associations and validaitons to job_post and review models

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -1,2 +1,5 @@
 class JobPost < ApplicationRecord
+    has_many :reviews
+
+    validates :post_type, :job_type, :description, :creator_name, :creator_email presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ApplicationRecord
+    belongs_to :job_post
+
+    validates :title, :reviewer_name presence: true
 end
