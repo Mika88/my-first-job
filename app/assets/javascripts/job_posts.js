416d23c2b99d4901d8bdad91376e9d2f5470d704
Mika88 <mikaspivak@gmail.com>
2019-08-06 11:46:15 -0700

    Create a constructor function that constructs a JobPost object, and prototype functions to handle the format of a post and post list

--- app/assets/javascripts/job_posts.js
+++ app/assets/javascripts/job_posts.js
@@ -18,3 +18,38 @@ const getJobPosts = () => {
     });
   })
 }
+
+function JobPost(jobPost){
+  this.id = jobPost.id
+  this.postType = jobPost.post_type
+  this.jobType = jobPost.job_type
+  this.description = jobPost.description
+  this.location = jobPost.location
+  this.salary = jobPost.salary
+  this.creatorName = jobPost.creator_name
+  this.creatorEmail = jobPost.creator_email
+}
+
+JobPost.prototype.postFormat = function() {
+  let postInfo = `
+    <h4>${this.postType}</h4>
+    <h3>${this.jobType}</h3>
+    <h4>Description</h4>
+    <p>${this.description}</p>
+    <h4>${this.location ? `Location: ${this.location}` : `` }</h4>
+    <h4>${this.salary ? `Charge: $${this.salary} per hour` : `` }</h4>
+    <h4>contact info</h4>
+    <h4>Name: ${this.creatorName}</h4>
+    <h4>Email: ${this.creatorEmail}</h4>
+  `
+  return postInfo
+}
+
+JobPost.prototype.postIndex = function() {
+  let postList = `
+    <b>${this.postType}</b>
+    <b>${this.jobType}</b>
+    <b>${this.location} </b>
+  `
+  return this.postList
+}
