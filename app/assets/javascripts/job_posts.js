4fa1bc3c09390842f6573ee18008ea29a00ab515
Mika88 <mikaspivak@gmail.com>
2019-08-06 16:35:00 -0700

    Refactor postFormat function in job_posts.js file

--- app/assets/javascripts/job_posts.js
+++ app/assets/javascripts/job_posts.js
@@ -18,7 +18,6 @@ const bindClickEvents = () => {
   $(document).on('click', '.post_link', function(event) {
     event.preventDefault()
     let id = $(this).data("id")
-    history.pushState(null, null, `/job_posts/${id}`)
     getJobPost(id)
   })
 }
@@ -72,15 +71,18 @@ function JobPost(jobPost){
 
 JobPost.prototype.postFormat = function() {
   let postInfo = `
-    <h4>${this.postType}</h4>
-    <h3>${this.jobType}</h3>
-    <h4>Description</h4>
+    <h5>Job Type</h5>
+    <h6>${this.jobType}</h6>
+    <h5>Job Description </h5>
     <p>${this.description}</p>
-    <h4>${this.location ? `Location: ${this.location}` : `` }</h4>
-    <h4>${this.salary ? `Charge: $${this.salary} per hour` : '' }</h4>
-    <h4>contact info</h4>
-    <h4>Name: ${this.creatorName}</h4>
-    <h4>Email: ${this.creatorEmail}</h4>
+    <h5>Details</h5>
+    <ul>
+      ${this.location ? `<li>Location: ${this.location}</li>` : '' }
+      ${this.salary ? `<li>Hourly Salary: $${this.salary}</li>` : '' }
+    </ul>
+    <h5>contact info</h5>
+    <h6><b>Name:</b> ${this.creatorName}</h6>
+    <h6><b>Email:</b> ${this.creatorEmail}</h6>
   `
   return postInfo
 }
