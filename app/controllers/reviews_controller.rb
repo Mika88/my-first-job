39cf08929a428de9a0c9a481c51c2f34702e3ca7
Mika88 <mikaspivak@gmail.com>
2019-08-09 09:48:41 -0700

    Add index, show and create actions to ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+    render json: @reviews
+  end
+
+  def show
+      @review = Review.find(params[:id])
+      render json: @review
+  end
+
+  def create
+      @review = Review.new(review_params)
+
+      if @review.save
+          render json: @review
+      else
+          render json: { errors: { message: "Review failed to save"}}
+      end
+  end
+
+  private
+
+  def review_params
+      params.permit(:title, :content, :reviewer_name, :job_post_id)
+  end
 end
